49184d915a5109a06f63a7beea31e34318400820
Oliver Debenc <oliver.x.debenc@aib.ie>
2016-02-02 10:00:18 +0000

    setting up project

---
+++ app/app.js
@@ -0,0 +1,16 @@
+(function(){
+	'use strict';
+
+	angular.module('AngularPhotogallery', [
+		'ngAnimate',
+		'ui.router',
+		'hmTouchEvents',
+
+		// components
+
+		// states
+		'login',
+
+	]);
+
+})();

fb6a978ee4558d903d074c9c95e5464ce83ddacd
Oliver Debenc <oliver.x.debenc@aib.ie>
2016-02-02 10:12:01 +0000

    setting up project

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+module.exports = function(grunt) {
+	
+	// Configurable paths for the application
+	var appConfig = {
+		app: 'app',
+		dist: 'dist',
+		bower: 'bower_components',
+		tmp: './.tmp',
+		pkg: grunt.file.readJSON('package.json')
+	};
+
+	// Define the configuration for all the tasks
+	grunt.initConfig({
+
+		// Project settings
+		config: appConfig
+	});
+
+};

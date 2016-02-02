a9054bfc1d529a1875538799da06ec9ea4b23972
Oliver Debenc <oliver.x.debenc@aib.ie>
2016-02-02 15:17:02 +0000

    setting up project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,84 @@
 'use strict';
 
 module.exports = function(grunt) {
-	
-	// Configurable paths for the application
-	var appConfig = {
-		app: 'app',
-		dist: 'dist',
-		bower: 'bower_components',
-		tmp: './.tmp',
-		pkg: grunt.file.readJSON('package.json')
-	};
 
-	// Define the configuration for all the tasks
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	// Tasks Configuration
 	grunt.initConfig({
 
-		// Project settings
-		config: appConfig
+		// Project Settings
+		config: {
+			app: 'app',
+			dist: 'dist',
+			bower: 'bower_components',
+			tmp: './.tmp',
+			pkg: grunt.file.readJSON('package.json')
+		},
+
+		clean: {
+			tmp: '<%= config.tmp %>'
+		},
+		copy: {
+			bower: {
+				files: [{
+					expand: true,
+					cwd: 'bower_components',
+					src: ['**/*.js', '!**/*min.js', '**/*.css', '!**/*min.css'],
+					dest: '<%= config.tmp %>/bower_components'
+				}]
+			}
+		},
+		less: {
+			tmp: {
+				files: {
+					'.tmp/assets/styles/main.css' : ['app/assets/styles/main.less']
+				}
+			}
+		},
+		watch: {
+			less: {
+				files: ['<%= config.app %>/**/*.less'],
+				tasks: ['less:tmp']
+			}
+		},
+		connect: {
+			options: {
+				port: 9000,
+				hostname: '0.0.0.0',
+				livereload: 35729
+			},
+			livereload: {
+				options: {
+					base: [
+						'<%= config.tmp %>',
+						'<%= config.app %>'
+					]
+				}
+			}
+		}
+
 	});
+
+
+	/* Custom Tasks */
+	grunt.registerTask('serve', 'Compile and start web server', function() {
+		var tasks = [
+			'clean:tmp',
+			'copy:bower',
+			'less:tmp',
+			'connect:livereload',
+			'watch'
+		];
+		grunt.task.run(tasks);	
+	});
+
+	grunt.registerTask('default', [
+		'serve'
+	]);
 
 };
